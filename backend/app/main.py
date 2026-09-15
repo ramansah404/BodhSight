3af52182a1db65f97011b700e9b1309af14a1868
Anil Mahato <anilmahatox13@gmail.com>
2026-09-15 16:51:59 +0530

    Email log in working through otp

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from app.core.config import settings
 import os
 from dotenv import load_dotenv
 
+# Triggering reload to pick up SENDER_EMAIL from .env
 load_dotenv(os.path.join(os.path.dirname(__file__), "../../.env"))
 
 app = FastAPI(title=settings.APP_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
